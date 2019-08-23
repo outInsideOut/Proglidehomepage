62d583084ce5ddce384847b52e0e651e0e2aa631
outInsideOut <43928714+outInsideOut@users.noreply.github.com>
2019-08-23 17:30:29 +0100

    update

--- SMTP_ContactForm.php
+++ SMTP_ContactForm.php
@@ -17,28 +17,34 @@
      * Message Configuration
      */
 
+     //set where email is sent from
      $mail->setFrom($_POST['mailFrom'],$_POST["name"]);
+     //Set where mail is sent to
      $mail->addAddress('insideoutwebdev@gmail.com', "me");
+     //set where reply should be sent to
      $mail->addReplyTo($_POST['mailFrom'],$_POST["name"]);
 
+     //take variable values from html form
      $name = $_POST["name"];
      $subject = $_POST["subject"];
      $mailFrom = $_POST["mailFrom"];
-     $message = $_POST["message"];
+     $message = nl2br($_POST["message"]);
 
      $mailTo = "finred@hotmail.co.uk";
      $headers = "From: ".$mailFrom;
-     $txt = $name." has sent you a message from the website:".$message;
+     // $txt = $name." has sent you a message from the website: ".$message;
+     $txt = "<h2 align=center>From: ".$name."<br>Email: ".$mailFrom."<br></h2>
+     <p font-size:1.5rem><b>Message:</b></p><p>".$message."</p>";
 
      $mail->isHTML(true);
-     $mail->Subject="Website Form Submission:".$subject;
+     $mail->Subject="Website Form Submission: ".$subject;
      $mail->Body=$txt;
 
-
-    if ($mail->send()) {
-        echo "Your message was sent successfully!";
-    } else {
-        echo "Mailer Error: " . $mail->ErrorInfo;
-    }
+     if ($mail->send()) {
+         echo "Your message was sent successfully!";
+     } else {
+         echo "Mailer Error: " . $mail->ErrorInfo;
+     }
+     header("Location: http://insideoutwebdev.hopto.org/contact.html");
   }
 ?>
